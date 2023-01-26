7183a0da149ecfc34b1805d0aa94a8cff9d695d8
Edvelasquezdepaz123 <123699793+Edvelasquezdepaz123@users.noreply.github.com>
2023-01-26 14:09:35 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let shootButton = document.querySelector(".option-one");
+let shootScreen = document.querySelector(".option-one-screen");
+let fightButton = document.querySelector(".option-two");
+let fightscreen = document.querySelector(".option-two-screen");
+
+
+
+
+
+
+
+//When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+shootButton.onclick=function(){
+shootScreen.style.display="block";
+};
+
+
+//When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+fightButton.onclick=function(){
+fightscreen.style.display="block";
+};
+
